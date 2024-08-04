ea86a2a78cb0019f35c6c131d1c4cb0e35f304f0
gustaS2 <nepomucenagustavo@gmail.com>
2024-08-03 21:22:29 -0300

    diversas modificações

--- eablog2/src/App.jsx
+++ eablog2/src/App.jsx
@@ -1,14 +1,14 @@
 import {Routes, Route} from 'react-router-dom';
 import Home from './components/Home';
+import Footer from './components/Footer'
 import './styles.global.css'
 
 function App() {
   return (
-    <>
     <div>
       <Home/>
+      <Footer />
     </div>
-    </>
   )
 }
 
